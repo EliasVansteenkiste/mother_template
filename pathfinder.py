e4a5d1d3fe800d5de5c83027d9d32f7210347d33
Elias Vansteenkiste <Elias.Vansteenkiste@gmail.com>
2017-09-28 13:23:28 +0200

    initial commit for mother template

--- pathfinder.py
+++ pathfinder.py
@@ -3,20 +3,13 @@ import utils
 import os
 
 
-
 with open('SETTINGS.json') as data_file:
     paths = json.load(data_file)
 
 METADATA_PATH = paths["METADATA_PATH"]
 utils.check_data_paths(METADATA_PATH)
 
-PREDICTIONS_PATH = paths["PREDICTIONS_PATH"]
-utils.check_data_paths(METADATA_PATH)
-
 DATA_PATH = paths["DATA_PATH"]
 utils.check_data_paths(DATA_PATH)
 
-# VALIDATION_SPLIT_PATH = paths["VALIDATION_SPLIT_PATH"]
-# if not os.path.isfile(VALIDATION_SPLIT_PATH):
-#     raise ValueError('no validation file')
 
